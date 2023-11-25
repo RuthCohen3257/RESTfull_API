f4ce2e03e9eda6f57920174749abd12cd74fe361
RuthCohen3257 <r0533153528@gmail.com>
2023-11-25 19:29:32 +0200

    add DataContext

---
+++ RESTful_API/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using RESTful_API.Entities;
+
+namespace RESTful_API
+{
+    public class DataContext
+    {
+        public List<Appointment> Appointmens { get; set; }
+        public List<Doctor> Doctors { get; set; }
+        public List<Patient> Patients { get; set; }
+    }
+}
